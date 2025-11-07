33644e7b1d24aa4cbabb8f0f6dbd913c324f363e
Razlex42 <razlex42@gmail.com>
2025-11-06 18:33:35 -0800

    Add debug logging to creative tab event handler
    - Log when event fires and which tab is being processed
    - Log isPresent() check result
    - Log when item is added or warn if not present
    - Helps diagnose why item isn't appearing in creative tab

--- src/main/java/com/wcholmes/landscaper/ModEvents.java
+++ src/main/java/com/wcholmes/landscaper/ModEvents.java
@@ -1,9 +1,11 @@
 package com.wcholmes.landscaper;
 
+import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
 
 /**
  * Handles mod bus events (initialization, registration, creative tabs).
@@ -11,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
  */
 @Mod.EventBusSubscriber(modid = Landscaper.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEvents {
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     /**
      * Adds mod items to creative mode tabs.
@@ -18,11 +21,19 @@ public class ModEvents {
      */
     @SubscribeEvent
     public static void buildCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        LOGGER.info("buildCreativeTabs called for tab: {}", event.getTabKey().location());
+
         // Add the Naturalization Staff to the tools tab
         if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            LOGGER.info("Processing TOOLS_AND_UTILITIES tab");
+            LOGGER.info("NATURALIZATION_STAFF.isPresent() = {}", Landscaper.NATURALIZATION_STAFF.isPresent());
+
             // Check if registry object is present to avoid timing issues
             if (Landscaper.NATURALIZATION_STAFF.isPresent()) {
+                LOGGER.info("Adding Naturalization Staff to creative tab");
                 event.accept(Landscaper.NATURALIZATION_STAFF.get());
+            } else {
+                LOGGER.warn("NATURALIZATION_STAFF is not present during creative tab building!");
             }
         }
     }
